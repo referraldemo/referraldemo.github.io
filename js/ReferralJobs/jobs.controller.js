235eba375e6676a22233027117f5299b7d4480fd
mayank <mayank.mishra@naukri.com>
2016-03-03 15:49:06 +0530

    updating controllers so that it can show job description

--- js/ReferralJobs/jobs.controller.js
+++ js/ReferralJobs/jobs.controller.js
@@ -13,14 +13,16 @@ referralApp.factory('jobsFactory', ['$http', '__url', function ($http, __url) {
                     "rewards": "900/-",
                     "loc": "Ahmedabad, Delhi/NCR(National Capital Region) ",
                     "salary": "Rupees 90,000 - 3,00,000",
-                    "exp": "1-6"
+                    "exp": "1-6",
+                    "desc": "Work on evolution of requirements along with business and product <br>Work and get exposure on varied technologies including PHP,Java, MySQL, NOSQL, Mongo, MemCached, Lucene, Symfony, Nginx, Sphinx,MVC Frameworks and other upcoming internet technologies<br>Responsible for coding, documenting and maintaining web applications in a fast paced project environment<br>Responsible for high end product development<br>Continuously upgrading and building new technology skills and methodologies<br>Innovate, research, propose and discuss your design with your peers."
                 },
                 "190116000001": {
                     "title": "Assistant Accountant",
                     "rewards": "",
                     "loc": "Ahmedabad, Delhi/NCR(National Capital Region)",
                     "salary": "Rupees 90,000 - 3,00,000",
-                    "exp": "2 - 4"
+                    "exp": "2 - 4",
+                    "desc": "1)	Selling online fresher hiring recruitment solutions (End to End Hiring Solutions) by assessing their business requirements.<br>2)	To identify, develop, support service and grow business in listed account in the region<br>3)	Make a strong and positive impression on existing and prospective clients, foster positive relationships at all levels, particularly senior management levels.<br>4)	Account mining & network with multiple stakeholders with prospect organization, build trust with key stakeholders<br>5)	Drive revenue by prospecting and building pipeline while building strong personal relationships with potential clients in the key account category<br>6)	Understand our clients business challenges and appropriately align our solutions to help achieve their goals. <br>7)	Meet with clients and potential prospects in the region on a regular basis to build and nurture the relationship as well as look out for future opportunities.<br>8)	Accountable for direct sales targets, consistent pipeline growth, renewals, upgrades & sales quota management<br>9)	To maintain and grow revenue in listed accounts (Farming Accounts) in the region.<br>10)	Ensure penetration of new products and renewal/upgrade in each account set <br>11)	Maintain and update enterprise account dockets and undertake regular CRM initiatives to keep the listed accounts warmed<br>12)	Liaising with the assigned Accounts, Account Mapping, and Strategic planning on each account towards revenue & transaction growth. <br>13)	Negotiation, freezing on commercials and closing of deals with necessary documentations.<br>14)	Client Servicing & ensure usage of inventories subscribed.<br>15)	Develop and execute on a strategic plan for the territory and create reliable forecasts<br>16)	Work to develop and circulate the set of best practices that will be the foundation of this growing team<br>17)	Listen to the needs of the market and share with Product and Marketing team."
                 }
             });
             // hacks
